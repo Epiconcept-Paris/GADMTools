20d06de76feafc493f4c87e71078ee061a5c1a19
jpKDO <kdo@zpmag.com>
2015-11-02 13:50:25 +0100

    Update sources de la lib

--- test/T_dots.R
+++ test/T_dots.R
@@ -9,8 +9,7 @@ dots(MAP, points=P, value="population", strate="governorate")
 # dots(MAP, points=P, color="green", title="Cas de Lechmaniose en Syrie")
 # dots(MAP, points=P, title="Cas de Lechmaniose en Syrie",
 #      subtitle = "Enquête du 10 Janvier 2015 au 10 Juillet 2015")
-# MAP <- gadm.loadCountries("SYR", level = 0)
-# dots(MAP, points=P, title="Cas de Lechmaniose en Syrie",
-#      subtitle = "Enquête du 10 Janvier 2015 au 10 Juillet 2015")
-# 
+MAP <- gadm.loadCountries("SYR", level = 0)
+dots(MAP, points=P, title="Cas de Lechmaniose en Syrie")
+
 
